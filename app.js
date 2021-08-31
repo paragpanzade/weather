dd457223544334e53572c8e6a962217ed69d386a
aditipolkam <aditipolkam@gmail.com>
2021-08-31 12:35:24 +0530

    UPDATED app to fetch temperature using pincode

--- app.js
+++ app.js
@@ -1,16 +1,30 @@
 const http = require('http');
 const express = require('express');
+//require('dotenv').config();
 
-const port = 8081;
+//const port = process.env.PORT;
+//const apikey = process.env.API_KEY;
+const apikey = require('./keys.js')
 
 const app = express();
-app.set('view engine','ejs');
 
-//processing requests
-app.get('/', (req, res)=>{
-    res.render('index', {title: 'Home'})
-    console.log('Server processed request.')
+app.set('view engine', 'ejs');
+app.use(express.urlencoded({extended: true}));
+
+app.get('/',(req, res)=>{
+    res.render('index', {title : 'Home'});
 })
 
-app.listen(port);
+app.post('/check-weather',(req, res)=>{
+    var pincode = req.body.pincode;
+    console.log(pincode);
+    var url= `https://api.openweathermap.org/data/2.5/weather?zip=${pincode},in&units=metric&appid=`+ apikey;
+    var request = require('request');
+    request(url,function(err, r, body){
+        const data = JSON.parse(body);
+        //console.log(body);
+        res.render('details',{title: 'Weather Details', data});
+    });
+})
 
+app.listen(8081);
