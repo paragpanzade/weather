e3988b2317ab388ea63f7ea41ec99c5684d73eaa
aditipolkam <aditipolkam@gmail.com>
2021-09-01 10:27:15 +0530

    ADDED nav bar and removed css

--- app.js
+++ app.js
@@ -15,6 +15,14 @@ app.get('/',(req, res)=>{
     res.render('index', {title : 'Home'});
 })
 
+app.get('/about',(req, res)=>{
+    res.render('about', {title : 'About'});
+})
+
+app.get('/check-weather/pincode',(req, res)=>{
+    res.render('pincode', {title: 'Pincode'})
+})
+
 app.post('/check-weather',(req, res)=>{
     var pincode = req.body.pincode;
     //console.log(pincode);
@@ -23,8 +31,12 @@ app.post('/check-weather',(req, res)=>{
     request(url,function(err, r, body){
         const data = JSON.parse(body);
         //console.log(body);
-        res.render('details',{title: 'Weather Details', data});
+        res.render('pincode',{title: 'Weather Details', data});
     });
+})
+
+app.use((req, res)=>{
+    res.status(404).render('404',  {title : '404'});
 })
 
 app.listen(port);
